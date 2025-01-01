5ef35e636caca481cce09a66caa4ab2492ee130d
ali <senyor.19011@gmail.com>
2025-01-01 17:11:55 +0300

    Add docker files and postgres configurations.

--- IHA/IHA/settings.py
+++ IHA/IHA/settings.py
@@ -74,10 +74,21 @@ WSGI_APPLICATION = 'IHA.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydatabase',
+        'USER': 'myuser',
+        'PASSWORD': 'mypassword',
+        'HOST': 'db',  # This should match the service name in the Docker Compose file
+        'PORT': '5432',
     }
 }
 
